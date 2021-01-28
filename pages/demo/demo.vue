a12383c3ed7d820e13426dd7aac1fb6370b1f2c2
Cunjun Wang <duckwcj@gmail.com>
2021-01-28 12:03:23 +0800

    创建登录页面

---
+++ pages/demo/demo.vue
@@ -0,0 +1,40 @@
+<template>
+  <view>
+    <view>{{ username }}</view>
+    <view v-for="t in tel">{{ t }}</view>
+    <view v-if="age >= 18">
+      <button @tap="signUp()">我要报名</button>
+    </view>
+    <view>
+      <input type="text" v-model="address" placeholder="输入地址"/>
+    </view>
+    <view>{{ address }}</view>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: "Duck",
+      tel: [
+        '12345678',
+        '87654321'
+      ],
+      age: 20,
+      address: ""
+    }
+  },
+  methods: {
+    signUp: function () {
+      uni.showToast({
+        title: "点击了报名按钮"
+      })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/login/login.vue
@@ -0,0 +1,26 @@
+<template>
+  <view>
+    <image src="../../static/logo-1.png" class="logo"
+           mode="widthFix"></image>
+    <view class="logo-title">EMOS在线办公系统</view>
+    <view class="logo-subtitle">Ver 2021.1</view>
+    <button class="login-btn" open-type="getUserInfo" @tap="login()">登录系统</button>
+    <view class="register-container">
+      没有账号?
+      <text class="register" @tap="toRegister()">立即注册</text>
+    </view>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<style lang="less">
+@import url("login.less");
+</style>
