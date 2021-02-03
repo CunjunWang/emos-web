02e9db9bbeb5950d9716be5d24b59f72763116c4
Cunjun Wang <duckwcj@gmail.com>
2021-02-03 22:38:33 +0800

    修复考勤统计页面的展示问题

--- pages/checkin_result/checkin_result.vue
+++ pages/checkin_result/checkin_result.vue
@@ -70,26 +70,18 @@
 export default {
   data() {
     return {
-      username: "王以太",
-      avatarUrl: "https://thirdwx.qlogo.cn/mmopen/vi_32/DYAIOgq83er164JNVcRibNUWjC6MIZcIvDk9ccxVUsckybHj9tCBaZXhmI5PPGnSlZvcmdKV5d5QEkyUQcXArkw/132",
-      deptName: "新零售事业群",
-      address: "上海市虹口区",
-      status: "正常",
-      risk: "中风险",
-      checkinTime: "08:25",
-      date: "2021年2月3日",
+      username: "",
+      avatarUrl: "",
+      deptName: "",
+      address: "",
+      status: "",
+      risk: "",
+      checkinTime: "",
+      date: "",
       attendanceTime: "08:30",
       closingTime: "09:30",
-      checkinDays: 201,
-      weeklyCheckin: [
-        {type: "工作日", day: "周一", status: "缺勤"},
-        {type: "工作日", day: "周二", status: "迟到"},
-        {type: "工作日", day: "周三", status: "正常"},
-        {type: "工作日", day: "周四", status: "正常"},
-        {type: "工作日", day: "周五", status: "正常"},
-        {type: "节假日", day: "周六", status: ""},
-        {type: "节假日", day: "周日", status: ""}
-      ]
+      checkinDays: 0,
+      weeklyCheckin: []
     }
   },
   onShow: function () {
