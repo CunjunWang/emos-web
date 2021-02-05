4e7c27b95c2fb05edaf6cc77b07fd57b43a862ef
Cunjun Wang <duckwcj@gmail.com>
2021-02-05 16:47:35 +0800

    设计静态的我的考勤页面

--- pages/my_checkin/my_checkin.vue
+++ pages/my_checkin/my_checkin.vue
@@ -1,13 +1,49 @@
 <template>
   <view>
-    我的考勤
+    <view class="stats">
+      <image src="../../static/big-icon-1.png" mode="widthFix" class="icon-big"></image>
+      <view class="report-title">
+        <text class="days">{{ sumOnTime }}</text>
+        <text class="unit">天</text>
+      </view>
+      <view class="sub-title">
+        <text>本月签到</text>
+      </view>
+      <view class="report">
+        <view class="column green">
+          <text class="column-title">正常签到</text>
+          <text class="number">{{ sumOnTime }}</text>
+        </view>
+        <view class="column yellow">
+          <text class="column-title">迟到天数</text>
+          <text class="number">{{ sumLate }}</text>
+        </view>
+        <view class="column red">
+          <text class="column-title">缺勤天数</text>
+          <text class="number">{{ sumAbsence }}</text>
+        </view>
+      </view>
+      <view class="calendar-container">
+        <uni-calendar :insert="true" :lunar="false" :selected="list"></uni-calendar>
+      </view>
+    </view>
   </view>
 </template>
 
 <script>
+import uniCalendar from '@/components/uni-calendar/uni-calendar.vue';
+
 export default {
+  components: {
+    uniCalendar
+  },
   data() {
-    return {}
+    return {
+      list: {type: Array, default: []},
+      sumOnTime: 0, // 准时签到
+      sumAbsence: 0, // 缺勤
+      sumLate: 0, // 迟到
+    }
   },
   methods: {}
 }
